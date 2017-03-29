984db28d6da8ca579ab5099197c16b4b56099fca
Harshal Bhatia <harshalbhatia@users.noreply.github.com>
2017-03-29 23:20:40 +0530

    Finished the site.

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
+  chiefs: Object[] =  [
+    {
+      name: 'Shri. Puneeth Rajkumar',
+      desc: 'Actor, Kannada Film Industry'
+    },
+    {
+      name: 'Shri. Santosh S. Lad',
+      desc: 'Hon\'ble Minister for Labour, Government of Karnataka' 
+    },
+    {
+      name: 'Shri. Rizwan Arshad',
+      desc: 'MLC, Youth Congress President, Karnataka'
+    },
+    {
+      name: 'Shri. Sridhar Pabbisetty',
+      desc: 'CEO, Namma Bengaluru Foundation'
+    },
+    {
+      name: 'Shri. VC Prakash',
+      desc: 'Deputy Chairman, NRI Forum of Karnataka'
+    },
+    {
+      name: 'Shri. KV Goutham',
+      desc: 'General Secretary, KPCC'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
